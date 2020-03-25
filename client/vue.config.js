5bcefb34bcd083878875dd9e27f3e506f2e3112e
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-03-25 14:44:29 +0800

    vue.config.js: reinitialize how to build to docs

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "vuetify"
   ],
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/client/'
+    ? '/client'
     : '/',
-  outputDir: 'docs'
+  outputDir: '../docs'
 }
