68220e7901ce3654396e566536c4cc30d81b6d9d
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-03-26 15:05:15 +0800

    Register: Revamped layout, also v-select

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -5,7 +5,7 @@
           <!-- <form class="regForm" action="index.html" method="POST"> -->
           <form class="regForm">
 
-              <h2 class="title">Registration</h2>
+              <h1 class="title">Registration</h1>
 
               <div class="inputs">
 
@@ -57,6 +57,39 @@
                         </div>
                     </div>
 
+                    <div class="input-div major">
+                        <div class="i">
+                        </div>
+                        <div class="div">
+                          <h5>Major </h5>
+                          <input type="text" class="input" name="regMajor">
+                        </div>
+                    </div>
+
+                    <!-- <div class="input-div select-div college">
+                        <div class="select">
+                          <select class="selector" name="collegeSelector">
+                            <option value="college">College</option>
+                            <option value="chungchi">Chung Chi College</option>
+                            <option value="newasia">New Asia College</option>
+                            <option value="united">United College</option>
+                            <option value="shaw">Shaw College</option>
+                            <option value="morningside">Morningside College</option>
+                            <option value="shho">S.H.Ho College</option>
+                            <option value="cwchu">CW.Chu College</option>
+                            <option value="wuyeesun">Wu Yee Sun College</option>
+                            <option value="leewoosing">Lee Woo Sing College</option>
+                          </select>
+                        </div>
+                    </div> -->
+                    <v-col class="d-flex" cols="22" sm="20">
+                      <v-select
+                        :items="colleges"
+                        label="Colleges"
+                        
+                      ></v-select>
+                    </v-col>
+
               </div>
 
               <!-- <button type="submit" class="btn">Register</button> -->
@@ -70,28 +103,42 @@
 
 <script>
 export default {
-    mounted() {
-        const inputs = document.querySelectorAll(".input");
-        function addcl(){
-            let parent = this.parentNode.parentNode;
-            parent.classList.add("focus");
-        }
+  data: () => ({
+    colleges: ['S.H.Ho College', 'New Asia College', 'Shaw College', 'Morningside College', 'Chung Chi College',
+              'United College', 'CW.Chu College', 'Wu Yee Sun College', 'Lee Woo Sing College'],
+  }),
 
-        function remcl(){
-            let parent = this.parentNode.parentNode;
-            if(this.value == ""){
-                parent.classList.remove("focus");
-            }
-        }
+  mounted() {
+      const inputs = document.querySelectorAll(".input");
+      function addcl(){
+          let parent = this.parentNode.parentNode;
+          parent.classList.add("focus");
+      }
 
-        inputs.forEach(input => {
-            input.addEventListener("focus", addcl);
-            input.addEventListener("blur", remcl);
-        });
-    }
+      function remcl(){
+          let parent = this.parentNode.parentNode;
+          if(this.value == ""){
+              parent.classList.remove("focus");
+          }
+      }
+
+      inputs.forEach(input => {
+          input.addEventListener("focus", addcl);
+          input.addEventListener("blur", remcl);
+      });
+  }
 }
 </script>
 
 <style scoped lang="scss">
 @import '../css/Page.css';
+
+.v-select__selections {
+  height: 90px !important;
+  background-color: black;
+}
+.v-text-field {
+  padding-top: 45px;
+}
+
 </style>
