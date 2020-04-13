85405f34dd930e80a4210567aef0637ca90ee1a6
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-04-13 21:24:03 +0800

    Firebase: Removal

--- client/src/main.js
+++ client/src/main.js
@@ -5,21 +5,21 @@ import store from './store'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify';
 
-import firebase from 'firebase';
+// import firebase from 'firebase';
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDqz0hcIvcrTNuDjFlnc8hWU9Mjynw1p9E",
-  authDomain: "csci3100-805c1.firebaseapp.com",
-  databaseURL: "https://csci3100-805c1.firebaseio.com",
-  projectId: "csci3100-805c1",
-  storageBucket: "csci3100-805c1.appspot.com",
-  messagingSenderId: "326474493349",
-  appId: "1:326474493349:web:8f70a0cdd9c565f3288c4c",
-  measurementId: "G-1FK4GFHKP2"
-};
+// var firebaseConfig = {
+//   apiKey: "AIzaSyDqz0hcIvcrTNuDjFlnc8hWU9Mjynw1p9E",
+//   authDomain: "csci3100-805c1.firebaseapp.com",
+//   databaseURL: "https://csci3100-805c1.firebaseio.com",
+//   projectId: "csci3100-805c1",
+//   storageBucket: "csci3100-805c1.appspot.com",
+//   messagingSenderId: "326474493349",
+//   appId: "1:326474493349:web:8f70a0cdd9c565f3288c4c",
+//   measurementId: "G-1FK4GFHKP2"
+// };
 
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
 Vue.config.productionTip = false
 
