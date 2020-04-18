921c3466a761820a2d4152c56794232b950018f1
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-04-19 00:12:23 +0800

    mistakes were made

---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="content">
+            <h1> Sorry, the page you're looking for does not exist  :c </h1>
+            <router-link to="/home"><button> Return to Home </button></router-link>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.content {
+    margin: auto;
+}
+button {
+    display: block;
+    margin: 10px auto 0 auto;
+    text-decoration: none;
+    color: white;
+    border-radius: 10px;
+    background-color: #6d9dff;
+    padding: 15px;
+}
+</style> 
