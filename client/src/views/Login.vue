82ee737045041add6020406cd2c3af2b8f79213d
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-04-19 00:11:07 +0800

    Login: changes??

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,56 +1,87 @@
 <template>
-    <v-app>
-        <div class="container">
+<!-- TODO: Create the animation and UI -->
+    <!-- <div class="login-page">
+        <div class="login-form" @submit.prevent="onLogin">
+            <form>
+                <h2 class="title">Log In</h2>
 
-            <div class="login-content">
-                <!-- <form class="loginForm" action="index.html" method="POST"> -->
-                <form class="loginForm">
+                <div class="email">
+                    <input type="text" placeholder="Email" v-model="email"/>
+                </div>
+                <div class="password">
+                    <input type="password" placeholder="Password" v-model="password"/>
+                </div>
 
-                    <h2 class="title">Log In</h2>
-
-                    <div class="input-div one">
-                        <div class="i">
-                            <i class="fas fa-at"></i>
-                        </div>
-                        <div class="div">
-                            <h5>Email</h5>
-                            <input type="email" class="input" name="loginEmail">
-                        </div>
-                    </div>
-
-                    <div class="input-div pass">
-                        <div class="i">
-                            <i class="fas fa-lock"></i>
-                        </div>
-                        <div class="div">
-                            <h5>Password</h5>
-                            <input type="password" class="input" name="loginPassword">
-                        </div>
-                    </div>
-
-                    <a href="#">Forgot Password?</a>
-                    <!-- <button type="submit" class="btn">Login</button> -->
-                    <!-- Link to home page -->
-                    <router-link to="/home"> <button class="btn">Login</button> </router-link>
-
-                </form>
-
-            </div>
-
-            <!-- Link to register -->
-            
-            <div class="registration">
-                Do not have account yet?
-                <router-link to="/register">Register Here</router-link>
-            </div>
-            
-
+                <button>Login</button>
+                <router-link to="/register"><button>Register Here</button></router-link>
+            </form>
+            <div class="error" v-if="error">{{ error.message }}</div>
         </div>
-    </v-app>
+    </div> -->
+
+    <div class="container">
+
+      <div class="login-content" @submit.prevent="onLogin">
+          <form class="loginForm">
+
+              <h2 class="title">Log In</h2>
+
+              <div class="input-div one">
+                  <div class="i">
+                    <i class="fas fa-at"></i>
+                  </div>
+                  <div class="div">
+                    <h5>Email</h5>
+                    <input type="text" class="input" v-model="email">
+                  </div>
+              </div>
+
+              <div class="input-div pass">
+                  <div class="i">
+                    <i class="fas fa-lock"></i>
+                  </div>
+                  <div class="div">
+                    <h5>Password</h5>
+                    <input type="password" class="input" v-model="password">
+                  </div>
+              </div>
+
+              <a href="#">Forgot Password?</a>
+              <button class="lgn">Login</button>
+
+          </form>
+
+        <div class="error" v-if="error">{{ error.message }}</div>
+
+      </div>
+
+      <div class="registration">
+        Do not have account yet?
+        <router-link to="/register">Register Here</router-link>
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            email: "",
+            password: "",
+            error: "",
+            authenticated: false,
+        }
+    },
+    methods: {
+        async onLogin() {
+            try {
+                const user = await handleSignIn(this.email, this.password)
+                this.$router.push("/home");
+            } catch(e) {
+                this.error = e;
+            }
+        }
+    },
     mounted() {
         const inputs = document.querySelectorAll(".input");
         function addcl(){
@@ -80,5 +111,11 @@ export default {
     a {
         display: inline-block;
     }
+}
+
+.error {
+    padding: 50px;
+    color: white;
+    font-size: 18px;
 }
 </style>
