854103a07b790f81ef2fc46c99bf269e335522ac
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-04-19 19:03:59 +0800

    main.js: Element-UI inclusion

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,11 @@ import store from './store'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify';
 import VeeValidate from 'vee-validate';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en'
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
