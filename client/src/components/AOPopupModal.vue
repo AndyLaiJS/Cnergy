96258cbc1708ba996e6aa4c7488b20f525b16178
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-04-22 10:56:51 +0800

    Initial for Activity Owner PopupModal

---
+++ client/src/components/AOPopupModal.vue
@@ -0,0 +1,84 @@
+
+<template>
+    <div class="text-center">
+        <button @click="opendialog()">More info</button>
+        <v-dialog
+            v-model="dialog"
+            width="500"
+        >
+
+            <v-card>
+                <v-card-title
+                    class="headline"
+                    primary-title
+                > Title
+                </v-card-title>
+                
+                <v-card-text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <button @click="dialog = false" id="greenbtn"> Join </button>
+                    <v-spacer></v-spacer>
+                    <button @click="dialog = false"> Ok </button>
+                    <v-spacer></v-spacer>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        dialog: false,
+      }
+    },
+    methods: {
+        opendialog() {
+            this.dialog = true;
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+#info-btn {
+    position: relative;
+}
+.text-center {
+    z-index: 9999;
+}
+#greenbtn {
+    border: 2px solid #67C23A;
+    color: #67C23A;
+}
+#greenbtn:hover {
+    background-color: #67C23A;
+    color: white;
+}
+
+button{
+    padding: 5px 30px;
+    border-radius: 50px;
+    outline: none;
+    border: 2px solid #4285F4;
+    box-shadow:  0px 3px silver;
+    color: #4285F4;
+    cursor: pointer;
+    transition: .1s;
+}
+button:hover {
+    background-color: #4285F4;
+    color: #fff;
+}
+button:active {
+    box-shadow: 0 1px silver;
+    transform: translateY(3px);
+}
+</style>
